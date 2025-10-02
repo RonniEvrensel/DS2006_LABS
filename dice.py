7b4ce1dc78ca1dee4597f0a675f5b33b091a8302
RonniEvrensel <ronni94@live.se>
2025-10-02 20:04:01 +0200

    LAB 1: grundläggande tärningsfunktioner

---
+++ dice.py
@@ -0,0 +1,47 @@
+import random
+
+def rollD4():
+ return random.randint(1, 4)
+def rollD6():
+ return random.randint(1, 6)
+def rollD8():
+    return random.randint(1, 8)
+def rollD12():
+   return random.randint(1, 12)
+def rollD20():
+    return random.randint(1, 20)
+def rollD100():
+   return random.randint(1, 100)
+ 
+ #all codes below deactivated
+ #d4 input("Press Enter to roll the D4") result = rollD4()
+ #  print("you rolled a",result , "on a D4")
+
+ #d6 input("Press Enter to roll the D6")
+ #  result = rollD6()
+#print("you rolled a", result ,"on a D6")
+
+#d8
+#input("Press Enter to roll the D8")
+#result = rollD8()
+#print("you rolled a", result, "on a D8")
+
+#d12
+#input("Press Enter to roll D12")
+#result = rollD12()
+#print("you rolled a", result, "on a D12")
+
+#d20
+#input("Press Enter to roll D20")
+#result= rollD20()
+#print("you rolled a", result, "on a D20")
+
+#d100
+#input("Press Enter to roll D100")
+#result= rollD100()
+#print("you rolled a", result , "on a D100")
+
+
+
+
+
